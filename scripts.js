d016658b88658d9ffbb4d5efb538dc7ae3299aed
chadwyck <jonescd@rose-hulman.edu>
2014-09-18 16:34:31 -0400

    Added parallax effect

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-$("document").ready(function(e) {
+$(document).ready(function(e) {
     $(".col").on("mousedown", function(e) {
         var x = e.pageX;
         var y = e.pageY;
@@ -14,4 +14,32 @@ $("document").ready(function(e) {
 
         $(this).append('<svg><g><circle class="circ-expand" cx="'+setX+'" cy="'+setY+'" r="'+1+'"></circle></g></svg>');
     });
+
+    $('body').jKit({ 
+            'plugins': {
+                date: { 'path': "plugins/datepicker/js/datepicker.js", 'fn': 'DatePicker' },
+                hint: { 'path': "plugins/jquery.ztinputhint-1.2.js", 'fn': 'ztInputHint' },
+                maxlength: { 'path': "plugins/maxlength/jquery.maxlength-min.js", 'fn': 'maxlength' },
+                confirm: { 'path': "plugins/jquery.confirm-1.3.js", 'fn': 'confirm' }
+            },
+            'replacements': {
+                'encode': specialEncodeCommand
+            }
+        });
 });
+
+
+    function specialEncodeCommand(that, type, options){
+
+        var s = this.settings; // we don't need this one, but I'll leave it here so you know how to get the plugin settings in case you need them
+        var $that = $(that);
+
+        this.executeCommand(that, type, options); // execute the original command
+
+        // now add some additional functionality:
+
+        if (options.format == 'uppercase'){
+            $that.html($that.html().toUpperCase());
+        }
+
+    }
